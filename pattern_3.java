26eabfe440de1005c15c4b2e6fa79e0a0a265cdb
Chethan Lakshman <bagichethan@gmail.com>
2026-08-17 15:13:10 +0530

    A2Z pattern3.

---
+++ pattern_3.java
@@ -0,0 +1,11 @@
+class pattern_3 {
+    public void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+
+            System.out.println();
+        }
+    }
+}
