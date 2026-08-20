9658833074290fdd3bbea17a77316ae112254101
Chethan Lakshman Bagi <bagichethan@gamil.com>
2026-08-20 21:49:28 +0530

    A2Z pattern6.

---
+++ pattern_6.java
@@ -0,0 +1,12 @@
+public class pattern_6 {
+    public void pattern1(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
