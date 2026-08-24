8340ff40d0b8cc60abf11a60259b22af2bac036f
Chethan Lakshman Bagi <bagichethan@gamil.com>
2026-08-24 19:14:32 +0530

    A2Z pattern7.

---
+++ pattern_8.java
@@ -0,0 +1,20 @@
+public class pattern_8 {
+    class Solution {
+        public void pattern8(int n) {
+            for(int i = 0; i < n; i++) {
+
+                // spaces
+                for(int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+
+                // stars
+                for(int j = 0; j < 2*n-2*i-1; j++) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+        }
+    }
+}
