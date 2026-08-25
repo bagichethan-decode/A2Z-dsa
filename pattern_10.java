a99ff509967a162e07551d4f3518fa06a61ff4f8
Chethan Lakshman Bagi <bagichethan@gamil.com>
2026-08-25 23:27:57 +0530

    A2Z pattern10.

---
+++ pattern_10.java
@@ -0,0 +1,20 @@
+public class pattern_10 {
+    class Solution {
+        public void pattern10(int n) {
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=1;j++){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+            for(int i = n-1;i>=1;i--){
+                for(int j =1;j <= i; j++){
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
