bb26160edf1319c8e633294f85ba401dfc2fae29
CHETHAN LAKSHMAN BAGI <bagichethan@gmail.com>
2026-09-02 20:25:37 +0530

    A2Z pattern 20.

--- pattern_20.java
+++ pattern_20.java
@@ -1,35 +1,46 @@
 class Solution {
     public void pattern20(int n) {
-        // Initial space count for the first row
-        int spaces = 2 * n - 2;
 
-        for (int i = 1; i <= 2 * n - 1; i++) {
-            // Determine number of stars for the current row
-            int stars = (i <= n) ? i : (2 * n - i);
+        // Upper half
+        for(int i = 1; i <= n; i++) {
 
-            // 1. Print left stars
-            for (int j = 1; j <= stars; j++) {
+            // Left stars
+            for(int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
-            // 2. Print middle spaces
-            for (int j = 1; j <= spaces; j++) {
+            // Spaces
+            for(int j = 1; j <= 2 * (n - i); j++) {
                 System.out.print(" ");
             }
 
-            for (int j = 1; j <= stars; j++) {
+            // Right stars
+            for(int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
-           
             System.out.println();
+        }
 
-            // Adjust space count for next iteration
-            if (i < n) {
-                spaces -= 2;
-            } else {
-                spaces += 2;
+        // Lower half
+        for(int i = n - 1; i >= 1; i--) {
+
+            // Left stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
+
+            // Spaces
+            for(int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
         }
     }
 }
