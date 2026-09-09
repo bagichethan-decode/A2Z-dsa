a45834ecb35287ac1275c40f508170c1ad6cb432
CHETHAN LAKSHMAN BAGI <bagichethan@gmail.com>
2026-09-09 22:00:53 +0530

    Implement Armstrong number check in Solution class

---
+++ z-armstrongnumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isArmstrong(int n) {
+
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum = sum + digit * digit * digit;
+            n = n / 10;
+        }
+
+        return original == sum;
+    }
+}
