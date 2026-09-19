ee4ea736a4806cbcaf8f7b153b8c0d10226f5d51
CHETHAN LAKSHMAN BAGI <bagichethan@gmail.com>
2026-09-19 22:54:16 +0530

    method

--- pattern_22.java
+++ pattern_22.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void pattern23(int n) {
+    public void pattern22(int n) {
 
         for (int i = 0; i < 2 * n - 1; i++) {
             for (int j = 0; j < 2 * n - 1; j++) {
